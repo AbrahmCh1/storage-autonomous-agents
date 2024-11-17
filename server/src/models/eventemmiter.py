764455c589c5b4490608cc999ae38cb7c3b9a911
Nicole Arana <A01642924@tec.mx>
2024-11-16 22:08:42 -0600

    code done?

--- server/src/models/eventemmiter.py
+++ server/src/models/eventemmiter.py
@@ -1,8 +1,22 @@
 from typing import Callable, Any
+import socket
+import json
 
 class EventEmitter():
     def __init__(self):
         self.event_handlers: dict[str, list[Callable[[str], Any]]] = {}
+
+        # Configura el socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('localhost', 65432))
+        self.sock.listen()
+
+        print("Esperando conexión...")
+        self.conn, self.addr = self.sock.accept()
+
+        if self.conn:
+            print('Conectado por', self.addr)
+
 
     def register_handler(self, type: str, handler: Callable[[str], Any]):
         if type not in self.event_handlers:
@@ -11,4 +25,12 @@ class EventEmitter():
         self.event_handlers[type].append(handler)
 
     def send_event(self, type: str, data: Any):
-        pass
+
+        event = {
+            "type": type,
+            "data": data
+        }
+
+        message = json.dumps(event).encode('utf-8')
+        self.conn.sendall(message)
+
