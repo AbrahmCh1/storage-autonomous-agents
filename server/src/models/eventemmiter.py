f1520099864d6aac864cd6889673d7c480b6fd95
Axel Padilla <adpadillar25@gmail.com>
2024-11-16 20:06:55 -0600

    event emmiter description

--- server/src/models/eventemmiter.py
+++ server/src/models/eventemmiter.py
@@ -3,6 +3,7 @@ from typing import Callable, Any
 class EventEmitter():
     def __init__(self):
         self.event_handlers: dict[str, list[Callable[[str], Any]]] = {}
+        # initialize the TCP connection
 
     def register_handler(self, type: str, handler: Callable[[str], Any]):
         if type not in self.event_handlers:
@@ -11,4 +12,6 @@ class EventEmitter():
         self.event_handlers[type].append(handler)
 
     def send_event(self, type: str, data: Any):
+        # send an event through current TCP connection with the following shape
+        # {"type": type, "data": {...}}
         pass
